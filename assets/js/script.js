0d07d1878c6ea0e087f3ecc627dcd7b8c627b0e1
Kathy Kang <105574653+KatSKang@users.noreply.github.com>
2022-08-05 00:04:56 -0400

    edited font color, forEach and saveBtn function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-var timeBlocks = document.querySelector('.container');
+var timeBlocks = $('.container');
+var hours = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM"]
 
 //shows current date and time in header
 window.onload = todaysDate();
@@ -8,79 +9,24 @@ function todaysDate() {
     setInterval(todaysDate,1000);
 }
 
-//variable for hours and task associated
-var myDay = [
-    {
-        hour: "9 AM",
-        time: 9
-    },
-    {
-        hour: "10AM",
-        time: 10
-    },
-    {
-        hour: "11 AM",
-        time: 11,
-    },
-    {
-        hour: "12 PM",
-        time: 12
-    },
-    {
-        hour: "1 PM",
-        time: 13
-    },
-    {
-        hour: "2 PM",
-        time: 14
-    },
-    {
-        hour: "3 PM",
-        time: 15
-    },
-    {
-        hour: "4 PM",
-        time: 16
-    },
-    {
-        hour: "5 PM",
-        time: 17
-    },
-    {
-        hour: "6 PM",
-        time: 18
-    }
-]
-
 //create row elements for the hours
-function makeMyDay() {
-    for (var i = 0; i < myDay.length; i++) {
-        var newRow = document.createElement('div');
-        var hourEl = document.createElement('div');
-        var taskEl = document.createElement('textarea');
-        var saveEl = document.createElement('button');
-        newRow.classList.add('row');
-        hourEl.classList.add('hour', 'col')
-        taskEl.classList.add('description', 'col-8');
-        saveEl.classList.add('saveBtn', 'col');
-        hourEl.textContent = myDay[i].hour;
-        saveEl.textContent = "save";
-        timeBlocks.append(newRow);
-        newRow.append(hourEl,taskEl,saveEl);
+hours.forEach((value, index) => {
+    var newRow = $('<div>').attr('class', 'row')
+    var hourEl = $('<div>').attr('class', 'hour col').text(value)
+    var textEl = $('<textarea>').val(localStorage.getItem(value))
+    var saveEl = $('<button>').attr('class', 'saveBtn col').text('SAVE')
 
-
-        //use the current hour to set attribute class as past,present, or future
-        if (myDay[i].time < moment().format('H')) {
-            taskEl.classList.add('past');
-        } else if (myDay[i].time == moment().format('H')) {
-            taskEl.classList.add('present');
-        } else {
-            taskEl.classList.add('future');
-        }
+    if (index + 9 < moment().format('H')){
+        textEl.attr('class', 'description col-9 past')
+    } else if (index + 9 == moment().format('H')){
+        textEl.attr('class', 'description col-9 present')
+    } else {
+        textEl.attr('class', 'description col-9 future')
     }
-}
 
-makeMyDay();
+    timeBlocks.append(newRow)
+    newRow.append(hourEl,textEl,saveEl)
+})
 
 //saves the text for the task in local storage
 $(document).ready(function () {
@@ -89,11 +35,7 @@ $(document).ready(function () {
         var hour = $(this).parent().children('.hour').text();
 
         localStorage.setItem(hour,memo);
-        console.log("value is ", memo);
-        console.log("hour is", hour);
+        console.log("The memo is", memo);
+        console.log("The hour is", hour);
     })
 }) 
-
-
-//TODO:
-//local storage: getItem
